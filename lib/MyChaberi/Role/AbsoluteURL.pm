f54bd4ae4027ab9ebb242ba88c23e94797d9417a
hiratara <hiratara@cpan.org>
2009-11-10 21:59:13 +0900

    Added AbsoluteURL role.

---
+++ lib/MyChaberi/Role/AbsoluteURL.pm
@@ -0,0 +1,16 @@
+package MyChaberi::Role::AbsoluteURL;
+use Moose::Role;
+use URI;
+
+requires 'request';
+
+sub abs_url {
+	my $self = shift;
+	my ( $rel_url ) = @_;
+	my $req  = $self->request;
+
+	return URI->new_abs( $rel_url, $req->uri )->as_string;
+}
+
+no  Moose::Role;
+1;
