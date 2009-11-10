e096ed890b4573f12094e9d3ff071b6e61f58949
hiratara <hiratara@cpan.org>
2009-11-10 17:41:00 +0900

    Hidden codes which connect to chaberi into a singleton class.

--- lib/MyChaberi/ChatRoomHandler.pm
+++ lib/MyChaberi/ChatRoomHandler.pm
@@ -1,11 +1,22 @@
 package MyChaberi::ChatRoomHandler;
 use Moose;
 use utf8;
+use MyChaberi::Connection;
 
 extends 'Tatsumaki::Handler';
 
 sub get {
     my $self = shift;
+
+    MyChaberi::Connection->connect(
+        address => 'ch0.chaberi.com', 
+        port    => '6899',
+        room    => '9',
+        name    => 'ゲスト39',
+        id      => '2626790',
+        hash    => '11eb725d6a4dfc5e31eb8f773e637bbf',
+    ) unless MyChaberi::Connection->instance;
+
     $self->render( 'chat.html' );
 }
 
