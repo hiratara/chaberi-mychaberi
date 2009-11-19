eeba250cab9a8cf0130e00815649e1bf38b7d28e
hiratara <hiratara@cpan.org>
2009-11-19 20:02:18 +0900

    Tatsumaki template was changed.

--- lib/MyChaberi/Application.pm
+++ lib/MyChaberi/Application.pm
@@ -8,9 +8,10 @@ sub BUILD {
 
 	# XXX backword compatible 
 	#     ( Tatsumaki b9161912b6fef4434a76fdcbca3d57ecea9cdbb7 )
-	$self->template->{tag_start} = "<?";
-	$self->template->{tag_end} = "?>";
-	$self->template->{line_start} = "?";
+	#     ( Tatsumaki 13eef7f7d4fb5b9141afc7f6d3ba3d8e800ccfd7 )
+	$self->template->mt->{tag_start} = "<?";
+	$self->template->mt->{tag_end} = "?>";
+	$self->template->mt->{line_start} = "?";
 }
 
 __PACKAGE__->meta->make_immutable;
